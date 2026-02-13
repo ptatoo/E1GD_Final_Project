4a497a659a7afff504e1ebf0ed229e39aad65caa
Arman Mkrtchyan <amkrtchyan1101@gmail.com>
2026-02-12 16:45:57 -0800

    A

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField] private int gamePhase; //0 day, 1 night
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
