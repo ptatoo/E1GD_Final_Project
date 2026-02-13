8213e22035d7afa6d2856550f512a8759c29ac35
Arman Mkrtchyan <amkrtchyan1101@gmail.com>
2026-02-12 17:49:00 -0800

    Event and Coroutine to change from day to night

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,19 +1,29 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.InputSystem; 
 
 public class GameManager : MonoBehaviour
 {
 
     [SerializeField] private int gamePhase; //0 day, 1 night
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private UnityEvent phaseChangeEvent; 
+    [SerializeField] private PlayerInput playerInput; 
+
+    void Awake()
     {
-        
+        StartCoroutine(DayPhaseCoroutine(gamePhase)); 
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator DayPhaseCoroutine (int phase)
     {
-        
+        for(int i = 1; i <= 5; i ++)
+        {
+            yield return new WaitForSeconds(1.0f); 
+            Debug.Log(i); 
+        }
+        phaseChangeEvent.Invoke(); 
+        playerInput.SwitchCurrentActionMap("Night"); 
     }
 }
