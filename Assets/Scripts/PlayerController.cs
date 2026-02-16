32ad668fb762fa38163476a5bb93dc5c490cbc90
Jose Elizalde <125120357+ElizaldeJoseCS@users.noreply.github.com>
2026-02-16 01:38:54 -0800

    Created my own scene and added night action map

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,43 +4,56 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float maxThreshold; 
-    [SerializeField] private float speed; 
-    private Vector2 inputDirection; 
+    [SerializeField] private float maxThreshold;
+    [SerializeField] private float speed;
+    private Vector2 inputDirection;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Debug.Log($"inputDirection = {inputDirection}");
-        Vector2 dPos = GetTranslationVector(); 
 
-        transform.Translate(dPos); 
+        // Debug.Log($"inputDirection = {inputDirection}");
+        Vector2 dPos = GetTranslationVector();
+
+        transform.Translate(dPos);
     }
 
     private Vector2 GetTranslationVector()
     {
         Vector2 dPos = inputDirection * speed * Time.deltaTime;
 
-        if(Mathf.Abs(transform.position.x + dPos.x) > maxThreshold)
+        if (Mathf.Abs(transform.position.x + dPos.x) > maxThreshold)
         {
-            dPos.x = 0f; 
+            dPos.x = 0f;
         }
 
-        if(Mathf.Abs(transform.position.y + dPos.y) > maxThreshold)
+        if (Mathf.Abs(transform.position.y + dPos.y) > maxThreshold)
         {
-            dPos.y = 0f; 
+            dPos.y = 0f;
         }
 
-        return dPos; 
+        return dPos;
     }
 
     private void OnMove(InputValue value)
     {
-        inputDirection = value.Get<Vector2>(); 
+        inputDirection = value.Get<Vector2>();
     }
+    bool checkCrouch()
+    {
+
+        if (Input.GetKey(KeyCode.C))
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+    
+
 }
