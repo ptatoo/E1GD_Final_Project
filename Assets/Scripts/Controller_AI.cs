c38b27b67a52aa9d8b1594d094eebd7a9888b1e3
Arman Mkrtchyan <amkrtchyan1101@gmail.com>
2026-03-04 13:33:09 -0800

    Rotating Vision Trigger Collider

--- Assets/Scripts/Controller_AI.cs
+++ Assets/Scripts/Controller_AI.cs
@@ -10,6 +10,9 @@ public class Controller_AI : MonoBehaviour
     [SerializeField] UnityEvent onNpnWakeUP;
     bool npcWakeUp = false;
 
+    [SerializeField] Vector3 lastPosition;
+    [SerializeField] Transform visionTransform;
+
 
 
     AILerp ai;
@@ -18,13 +21,50 @@ public class Controller_AI : MonoBehaviour
     {
         ai = GetComponent<AILerp>();
         ai.destination = dest2.position;
+        lastPosition = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 diff = transform.position - lastPosition; 
+        lastPosition = transform.position;
+        diff = diff.normalized;
 
-        Debug.Log("Distance from d1: "+(transform.position - dest1.position).magnitude);
+        if (diff == Vector3.up)
+        {
+            visionTransform.eulerAngles = Vector3.zero;
+        }
+        else if (diff == Vector3.down)
+        {
+            visionTransform.eulerAngles = new Vector3(0, 0, 180); 
+        }
+        else if (diff == Vector3.right)
+        {
+            visionTransform.eulerAngles = new Vector3(0, 0, 270);
+        }
+        else if (diff == Vector3.left)
+        {
+            visionTransform.eulerAngles = new Vector3(0, 0, 90);
+        }
+        else if (diff.x > 0 && diff.y > 0) //Up Right
+        {
+            visionTransform.eulerAngles = new Vector3(0, 0, 315);
+        }
+        else if (diff.x < 0 && diff.y > 0) //Up Left
+        {
+            visionTransform.eulerAngles = new Vector3(0, 0, 45); 
+        }else if(diff.x > 0 && diff.y < 0) //Down Right
+        {
+            visionTransform.eulerAngles = new Vector3(0, 0, 225); 
+        }else if(diff.x < 0 && diff.y < 0) //Down Left
+        {
+            visionTransform.eulerAngles = new Vector3(0, 0, 135);
+        }
+
+
+
+            Debug.Log("Distance from d1: " + (transform.position - dest1.position).magnitude);
                 Debug.Log("Distance from d2: "+(transform.position - dest2.position).magnitude);
         if (npcWakeUp)
             ai.destination = playerPos.position;
